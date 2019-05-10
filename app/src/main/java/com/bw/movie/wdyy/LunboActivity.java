a9618d948a4823e9ea840e47568e42146d9fe05f
Administrator <1686625458@qq.com>
2019-05-10 20:35:25 +0800

    尚义鹏提交

--- app/src/main/java/com/bw/movie/wdyy/LunboActivity.java
+++ app/src/main/java/com/bw/movie/wdyy/LunboActivity.java
@@ -1,17 +1,24 @@
 package com.bw.movie.wdyy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LunboActivity extends AppCompatActivity {
-    List<View> list=new ArrayList<>();
+    List<View> list = new ArrayList<>();
+    @BindView(R.id.viewpager_id)
+    ViewPager viewpager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunbo);
+        ButterKnife.bind(this);
     }
 }
